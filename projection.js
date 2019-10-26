4f58faaeaca7f83fbd15bf35f87d30fbc2be0e70
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2019-10-26 11:16:19 -0500

    Ajax is starting to to work, haven't touched css or html

--- projection.js
+++ projection.js
@@ -1,13 +1,21 @@
-$(window).load(function(event) {
+$(document).ready(function(event) {
     var queryURL = "https://api.fantasy.nfl.com/v1/players/stats?statType=weekProjectedStats&season=2019&currentWeek&position=DEF&format=json";
     console.log(queryURL);
-    var input: tampa bay
     $.ajax({
         url: queryURL,
         type: "GET",
     }).then(function(response) {
         console.log(response);
-        $("#projection").html("<h1>" + response.players[name: "input"].name + "</h1>");
-        console.log(response.players[0].name);
+        var topScore = 0;
+        var playerIndex;
+        for (var i = 0; i < response.players.length; i++) {
+            if (response.players[i].weekProjectedPts > topScore) {
+                topScore = response.players[i].weekProjectedPts
+                playerIndex = i;
+            }
+        }
+        console.log(response.players[playerIndex].name);
+        // $("#projection").html(response.players[0].name + "<br>" + "Position: " + response.players[0].position + "<br>" + "Projected weekly points: " + response.players[0].weekProjectedPts + "<br>" + "Projected season points: " + response.players[0].seasonProjectedPts);
+        // console.log(response.players[0].name);
     });
 });
