8e81c77c9de82205774af17231f55149a112f39e
maixiong29 <54956325+maixiong29@users.noreply.github.com>
2019-10-28 21:23:37 -0500

    search player works and animation

--- searchcolumn.js
+++ searchcolumn.js
@@ -1,48 +1,68 @@
-
-
 $("#select-player").on("click", function() {
 
 
+    var playerName = $("#player-search").val().trim();
+   
 
-
- 
-    var queryURL = "https://api.fantasy.nfl.com/v1/players/stats?statType" 
- 
+    var playerName = $(this).attr("data-name");
+    var queryURL = "https://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2019&format=json" 
+    
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
+        var playerName = $("#player-search").val().trim();
+        console.log(playerName);
+        for (var i = 0; i < response.players.length; i++) {
+            if (response.players[i].name == playerName) {
+                console.log("i'm in the if statement");
+                playerName = response.players[i].name;
+            console.log(response.players[i].position);
+            
+            $(".name").html("<h3>" + response.players[i].name + "</h3>");
+            $(".position").html("<h3>" + "Position: " + response.players[i].position + "</h3>");
+            $(".team").html("<h3>" + "Team: " + response.players[i].teamAbbr + "</h3>");
+            $(".weekly-projected").html("<h3>" + "Weekly Projected Points: " + response.players[i].weekProjectedPts + "</h3>");
+            $(".season-projected").html("<h3>" + "Season Projected Points: " + response.players[i].seasonProjectedPts + "</h3>");
+            $(".season-points").html("<h3>" + "Points Scored this Season: " + response.players[i].seasonPts + "</h3>");
 
-        console.log(response);
+            var textWrapper = document.querySelector('.ml11 .letters');
+            textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+            
+            anime.timeline({loop: true})
+              .add({
+                targets: '.ml11 .line',
+                scaleY: [0,1],
+                opacity: [0.5,1],
+                easing: "easeOutExpo",
+                duration: 700
+              })
+              .add({
+                targets: '.ml11 .line',
+                translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+                easing: "easeOutExpo",
+                duration: 700,
+                delay: 100
+              }).add({
+                targets: '.ml11 .letter',
+                opacity: [0,1],
+                easing: "easeOutExpo",
+                duration: 600,
+                offset: '-=775',
+                delay: (el, i) => 34 * (i+1)
+              }).add({
+                targets: '.ml11',
+                opacity: 0,
+                duration: 1000,
+                easing: "easeOutExpo",
+                delay: 1000
+              });
+            }
+        }
         
-        var statsDiv = $("<div class='playerName'>");
 
-        var position = response.players.position;
-        var pOne = $("<p>").text("Position: " + position);
+    })
 
-        statsDiv.append(pOne);
-
-        var team = response.plays.teamAbbr;
-        var pTwo = $("<p>").text("Team Abbr: " + team);
-
-        statsDiv.append(pTwo);
-
-        var seasonProjected = response.seasonProjectedPts;
-        var pThree = $("<p>").text("Projected Points This Season: " + seasonProjected);
-
-        statsDiv.append(pThree);
-
-        var weekProjected = response.weekProjectedPts;
-        var pFour = $("<p>").text("Projected Points This Week: " + weekProjected);
-
-        statsDiv.append(pFour);
-
-        $("#stats-view").prepend(statsDiv);
-
-    });
 })
-
-
-
 
 
